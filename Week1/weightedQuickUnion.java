f898df76f5e830752ab73eaaed4e5aa13669703b
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-06-22 16:11:55 -0500

    added an improved version of quick-union

---
+++ Week1/weightedQuickUnion.java
@@ -0,0 +1,38 @@
+class WeightedQuickUnion {
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQuickUnion(int N)
+    {
+	id = new int[N];
+	sz = new int[N];
+	for(int i = 0; i < N; i++)
+	{
+	        id[i] = i;
+	}
+    }
+
+    public connected(int p, int q)
+    {
+	return root(p) == root(q);
+    }
+
+    public union(int p, int q)
+    {
+	int rootP = root(p);
+	int rootQ = root(q);
+
+	if(id[rootP] == id[rootQ])
+	    return;
+	if(sz[rootP] < sz[rootQ])
+        {
+	    id[rootP] = rootQ;
+	    sz[rootQ] += sz[rootP];
+	}
+	else
+	{
+	    id[rootQ] = rootP;
+	    sz[rootP] = rootQ;
+	}
+    }
+    
