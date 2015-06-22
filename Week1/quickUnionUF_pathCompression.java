b49eb93d91e07cb2cb0cf546ffb14a32dbd2cdc0
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-06-22 16:56:18 -0500

    added weighted quick union with path compression

---
+++ Week1/quickUnionUF_pathCompression.java
@@ -0,0 +1,48 @@
+class WeightedQuickUnion {
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQuickUnion(int N)
+    {
+	id = new int[N];
+	sz = new int[N];
+	for(int i = 0; i < N; i++)
+	{
+	        id[i] = i;
+	}
+    }
+
+    public connected(int p, int q)
+    {
+	return root(p) == root(q);
+    }
+
+    private root(int p)
+    {
+	while(i != id[i])
+	{
+	    id[i] = id[id[i]]; // make every other node in path point to its grandparents (thereby halving path length)
+	    i = id[i];
+	}
+	return i;
+    }
+    
+		 
+    public union(int p, int q)
+    {
+	int rootP = root(p);
+	int rootQ = root(q);
+
+	if(id[rootP] == id[rootQ])
+	    return;
+	if(sz[rootP] < sz[rootQ])
+        {
+	    id[rootP] = rootQ;
+	    sz[rootQ] += sz[rootP];
+	}
+	else
+	{
+	    id[rootQ] = rootP;
+	    sz[rootP] = rootQ;
+	}
+    }
