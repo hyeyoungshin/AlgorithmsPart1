5133b049020a3ed53d849ff8c70f06e540447a66
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2015-06-22 21:31:56 -0500

    renamed some files

---
+++ Week1/quickUnionOnePassVariant.java
@@ -0,0 +1,48 @@
+class WeightedQuickUnionImproved {
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQuickUnionImproved(int N)
+    {
+	id = new int[N];
+	sz = new int[N];
+	for(int i = 0; i < N; i++)
+	{
+	        id[i] = i;
+	}
+    }
+
+    public boolean connected(int p, int q)
+    {
+	return root(p) == root(q);
+    }
+
+    private int root(int p)
+    {
+	while(i != id[i])
+	{
+	    id[i] = id[id[i]];
+	    i = id[i];
+	}
+	return i;
+    }
+    
+		 
+    public void union(int p, int q)
+    {
+	int rootP = root(p);
+	int rootQ = root(q);
+
+	if(id[rootP] == id[rootQ])
+	    return;
+	if(sz[rootP] < sz[rootQ])
+        {
+	    id[rootP] = rootQ;
+	    sz[rootQ] += sz[rootP];
+	}
+	else
+	{
+	    id[rootQ] = rootP;
+	    sz[rootP] = rootQ;
+	}
+    }
---
+++ Week1/quickUnionTwoPass.java
@@ -0,0 +1,34 @@
+public class QuickUnionUF
+{
+    private int[] id;
+
+    public QuickUnionUF(int N)
+    {
+	id = new int[N};
+	for (int i = 0; i < N; i++)
+	{
+	    id[i] = i;
+	}
+    }
+
+    private int root(int i)
+    {
+	int root = i;
+	while(root != id[i]) root = id[i];
+        while(i != id[i]) id[i] = root;
+	return root;
+    }
+
+    public boolean connected(int p, int q)
+    {
+	return root(p) == root(q);
+    }
+
+    public void union(int p, int q)
+    {
+	int rootP = root(p);
+	int rootQ = root(q);
+
+	id[rootP] = rootQ;
+    }
+}
